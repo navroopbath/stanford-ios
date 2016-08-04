bb95bbed2718212f43755a93f53e83bbba3b431c
Navroop Bath <nbath@guidewire.com>
2016-08-03 19:41:24 -0700

    Project 2: Add the ->M and M buttons to the storyboard. Wire up M to the ViewController.

--- projects/proj2/proj1/ViewController.swift
+++ projects/proj2/proj1/ViewController.swift
@@ -12,8 +12,12 @@ class ViewController: UIViewController {
     
     @IBOutlet private weak var display: UILabel!
     @IBOutlet private weak var sequenceOfOps: UILabel!
+    private var opContainsVariable = false
     
     private var userIsInTheMiddleOfTyping = false
+    
+    // TODO should tapping "M" trigger touchDigit? This may cause problems if there is already an actual
+    // digit in the display and the user then presses "M"
     
     @IBAction private func touchDigit(sender: UIButton) {
         let digit = sender.currentTitle!
@@ -34,32 +38,43 @@ class ViewController: UIViewController {
         }
     }
     
+    @IBAction private func touchVariable(sender: UIButton) {
+        if !userIsInTheMiddleOfTyping {
+            display.text = sender.currentTitle!
+            brain.setOperand(display.text!)
+            opContainsVariable = true
+        }
+        displayValue = brain.result
+    }
+    
     private func isDecimalValid() -> Bool {
         return !display.text!.containsString(".")
     }
 
     private var displayValue : Double {
         get {
-            return Double(display.text!)!
+            return (Double(display.text!))!
         }
         set {
             display.text = String(newValue)
         }
     }
+
     
     var brain = CalculatorBrain()
     
     @IBAction private func performOperation(sender: UIButton) {
-        if userIsInTheMiddleOfTyping {
+        if userIsInTheMiddleOfTyping && !opContainsVariable { // setOperand already called if operation contains a variable
             brain.setOperand(displayValue)
         }
         userIsInTheMiddleOfTyping = false
+        opContainsVariable = false
         if let mathematicalSymbol = sender.currentTitle {
             brain.performOperation(mathematicalSymbol)
         }
         let description = brain.getDescription
         sequenceOfOps.text = description == "" ? " " : description
-        displayValue = brain.result
+        displayValue = brain.result // TODO ensure that the brain updates result after performing the M operation and that the displayValue is updates
     }
 }
 
